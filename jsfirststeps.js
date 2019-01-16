b7bd15da5f48b8d97f5b76ee4e9a9497ed5d91e3
yoshimitsoul <openclasspierre@gmail.com>
2019-01-16 11:04:12 +0100

    essais de calculs basiques avec variables et concaténations.

--- jsfirststeps.js
+++ jsfirststeps.js
@@ -1,1 +1,19 @@
-alert('Hello world!'); /*fonction alert qui ouvre une fenêtre de message avec le paramêtre hello world*/
+var nomUtilisateur = prompt('Tapez votre prenom: '); //invite à tapez son prénom)
+
+
+var salutation = 'Bienvenue ';
+salutation += nomUtilisateur; //Salutation personnalisée avec nom de l'utilisateur
+alert(salutation);
+
+alert('Nous allons faire un peu de calcul pour votre shopping'); //fenêtre d'explication
+
+var prixArticle = prompt('Tapez le prix de votre article');
+
+var reduction = prompt('Tapez le pourcentage de reduction'); 
+
+
+var remise = prixArticle * reduction / 100;
+var prixFinale = prixArticle - remise;
+
+alert('le prix de votre article est de : ' + prixFinale + ' euros'); //affcihe le prix après reduction de l'article
+
